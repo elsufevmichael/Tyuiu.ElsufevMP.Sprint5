0096942614bdc54e43f560384ae60aa6f4baf19d
Tyuiu.ElsufevMP@outlook.com <mihal.els.32@gmail.com>
2023-11-17 19:20:07 +0500

    Add project files.

---
+++ Tyuiu.ElsufevMP.Sprint5.Task0.V24.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ElsufevMP.Sprint5.Task0.V24.Lib
+{
+    public class DataService : ISprint5Task0V24
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            double z = (Math.Pow((double)x, 3) - 8) / (2 * Math.Pow((double)x, 2));
+            z = Math.Round(z, 3);
+            File.WriteAllText(path, Convert.ToString(z));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.ElsufevMP.Sprint5.Task0.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElsufevMP.Sprint5.Task0.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ElsufevMP.Sprint5.Task0.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f941e4ff-07a2-405e-a18c-3c97f2a646de")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
